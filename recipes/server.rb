0dd05ebbf2e5a29cb6231af08769ede7d267d027
root <root@ip-172-31-57-13.ec2.internal>
2015-05-21 20:22:53 +0000

    adding info to the html

--- recipes/server.rb
+++ recipes/server.rb
@@ -1,7 +1,10 @@
 package "apache2"
 
 file "/var/www/html/index.html" do
-  content "<h1>Hello, world!</h1>"
+  content "<h1>Hello, world!</h1>
+  IPADDRESS: #{node["ipaddress"]}
+  HOSTNAME : #{node["hostname"]}
+"
 end
 
 service "apache2" do
